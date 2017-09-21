fca1122c55b6c26882b23dca9f56d58e9682471d
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-09-21 11:48:08 +0300

    connect to the api and fetch data

--- src/App.js
+++ src/App.js
@@ -2,24 +2,58 @@ import React from 'react';
 
 import './App.css';
 
-const list = [
-    {
-      title: 'React',
-      url: 'https://facebook.github.io/react/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://github.com/reactjs/redux',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-];
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+console.log(url);
+
+// const list = [
+//     {
+//         title: 'React',
+//         url: 'https://facebook.github.io/react/',
+//         author: 'Jordan Walke',
+//         num_comments: 3,
+//         points: 4,
+//         objectID: 0,
+//     },
+//     {
+//         title: 'Redux',
+//         url: 'https://github.com/reactjs/redux',
+//         author: 'Dan Abramov, Andrew Clark',
+//         num_comments: 2,
+//         points: 5,
+//         objectID: 1,
+//     },
+//     {
+//         title: 'RxJs',
+//         url: 'https://github.com/reactjs/redux',
+//         author: 'Raks',
+//         num_comments: 2,
+//         points: 5,
+//         objectID: 2,
+//     },
+//     {
+//         title: 'Flux',
+//         url: 'https://github.com/reactjs/redux',
+//         author: 'Dds',
+//         num_comments: 2,
+//         points: 5,
+//         objectID: 3,
+//     },
+//     {
+//         title: 'MobX',
+//         url: 'https://github.com/reactjs/redux',
+//         author: 'dsk',
+//         num_comments: 2,
+//         points: 5,
+//         objectID: 4,
+//     },
+// ];
+
+
 
 const isSearched = (searchTerm) => (item) => {
     return !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -29,16 +63,34 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            list,
-            searchTerm: '',
+            result: null,
+            searchTerm: DEFAULT_QUERY,
         };
 
+        this.setSearchTopStories = this.setSearchTopStories.bind(this);
+        this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
         this.onDismiss = this.onDismiss.bind(this);
         this.onSearchChange = this.onSearchChange.bind(this);
     }
 
+    setSearchTopStories(result){
+        this.setState({result});
+    }
+
+    fetchSearchTopStories(searchTerm){
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+            .then(response => response.json())
+            .then(result => this.setSearchTopStories(result))
+            .catch(e => e);
+    }
+
+    componentDidMount(){
+        const {searchTerm} = this.state;
+        this.fetchSearchTopStories(searchTerm);
+    }
+
     onDismiss(id){
-        const updatedList = this.state.list.filter(item => item.objectID !== id);
+        const updatedList = this.state.result.hits.filter(item => item.objectID !== id);
         this.setState({list: updatedList});
     }
 
@@ -47,8 +99,12 @@ class App extends React.Component {
     }
 
     render() {
+        
 
-        const {searchTerm, list} = this.state;
+        const {searchTerm, result} = this.state;
+        if(!result){
+            return null;
+        }
 
         return (
             <div className="page">
@@ -62,7 +118,7 @@ class App extends React.Component {
                     </Search>
                 </div>
                 <Table 
-                    list={list}
+                    list={result.hits}
                     pattern={searchTerm}
                     onDismiss={this.onDismiss}
                 />
@@ -71,20 +127,6 @@ class App extends React.Component {
     }
 }
 
-// class Search extends React.Component {
-//     render(){
-//         const {value, onChange} = this.props;
-//         return (
-//             <form>
-//                 <input 
-//                     type='text' 
-//                     value={value}
-//                     onChange={onChange}
-//                 />
-//             </form>
-//         );
-//     }
-// }
 
 const Search = ({value, onChange, children}) => {
     return(
@@ -99,34 +141,6 @@ const Search = ({value, onChange, children}) => {
     );
 }
 
-
-// class Table extends React.Component {
-//     render(){
-//         const {list, pattern, onDismiss} = this.props;
-//         return(
-//             <div>
-//                 {list.filter(isSearched(pattern)).map((item)=>
-//                     <div key={item.objectID}>
-//                         <span>
-//                             <a href={item.url}>{item.title}</a>
-//                         </span>
-//                         <span>{item.author}</span>
-//                         <span>{item.num_comments}</span>
-//                         <span>{item.points}</span>
-//                         <span>
-//                             <button
-//                                 onClick={ () => onDismiss(item.objectID)}
-//                                 type="button"
-//                             >
-//                                 Dismiss
-//                             </button>
-//                         </span>
-//                     </div>                    
-//                 )}
-//             </div>
-//         );
-//     }
-// }
 
 const Table = ({list, pattern, onDismiss}) => {
     return(
